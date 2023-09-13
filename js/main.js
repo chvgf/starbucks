f2547c9f296f5fb88fb53d776fc30d1d3b75a97d
chvgf <chvgf@naver.com>
2023-09-13 20:09:40 +0900

    스타벅스 클론코딩

--- js/main.js
+++ js/main.js
@@ -20,3 +20,48 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', ''); // setAttribute: HTML속성을 추가하는 메소드 (왼쪽 속성, 오른쪽 출력될 값)
 });
+
+// 스크롤 시 전역 배지 (고정 배너) 숨기기
+// 페이지 스크롤에 따른 요소 제어
+const badgeEl = document.querySelector('header .badges');
+
+// 페이지에 스크롤 이벤트를 추가!
+// window: 브라우저 창 객체
+window.addEventListener('scroll', function () {
+ // (y축으로 얼마나 스크롤 했는 지) 페이지 스크롤 위치
+console.log(window.scrollY);
+  // Quiz:
+  //페이지 스크롤 위치가 500px을 넘으면 배지 요소 숨기고,
+  //페이지 스크롤 위치가 500px을 넘지 않으면 배지 요소를 보이기
+  if (window.scrollY > 500) {
+    // badgeEl.style.display = 'none'; 
+
+    // gsap적용
+    // gsap.to(요소, 지속시간, 옵션: {}) 메소드: css속성을 통해 애니메이션 처리
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // badgeEl.style.display = 'block';
+
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+});
+
+// 순차적으로 VISUAL 섹션 내 요소 보이기
+// 나타날 요소(.fade-in)들을 찾기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+// console.log(fadeEls);
+// 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach(function (fadeEl, index) {
+// gsap.to(요소, 지속시간, 옵션: {})
+  gsap.to(fadeEl, 1, {
+    opacity: 1,
+    delay: (index + 1) * 0.7 // 0.7s, 1.4s, 2.1s, 2.8s
+    // delay: 몇 초 뒤에 실행될 것 인가?
+  });
+});
