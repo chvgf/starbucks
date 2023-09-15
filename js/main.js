db4eaf9517af99c227812c0a7e7a9531d9d51116
chvgf <chvgf@naver.com>
2023-09-15 20:16:16 +0900

    23-09-15 스타벅스 끝

--- js/main.js
+++ js/main.js
@@ -149,3 +149,15 @@ gsap.to('.floating3', 2.5, {
   yoyo: true, //한 번 재생된 애니메이션을 다시 뒤로 재생
   ease: Power1.easeInOut //타이밍 함수 적용
 });
+
+// scrollMagic 사용
+// 그 외 crollreveal 등..
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({ // 감시할 장면(Scene) 추가 및 옵션 지정
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8 // 화면에 80%지점에서 보여짐 여부를 감시(0~1사이 값)
+  })
+  .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+});
